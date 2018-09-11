17f979f2694a1d4a99c65a8363919683109b8034
rehganavon <rehganavon>
2018-09-11 20:24:43 +0000

    Put test

--- models/test.py
+++ models/test.py
@@ -1,20 +1,6 @@
 # fastscore.input: gbm_input
 # fastscore.output: gbm_output
 
-
-
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.pipeline import Pipeline
-
-import imp
-
-def begin():
-    FeatureTransformer = imp.load_source("FeatureTransformer",
-                                         "score_auto_gbm/FeatureTransformer.py")
-    global gbmFit
-    with open("score_auto_gbm/gbmFit.pkl", "rb") as pickle_file:
-        gbmFit = cPickle.load(pickle_file)
-
 def action(datum):
     score = list(gbmFit.predict(pd.DataFrame([datum])))[0]
     yield score
